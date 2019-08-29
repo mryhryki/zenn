50af5c2fdefdb334cac0836762691b9621d1fd3d
hyiromori <hyiromori@gmail.com>
2019-08-29 15:17:28 +0900

    Update ServiceWorker & meta info.

--- service_worker.js
+++ service_worker.js
@@ -1,66 +1,50 @@
-const CacheVersion = "2019-08-01T00:51:32.880Z";
-const UrlsToCache = [
-  "/",
-  "/blog/",
-  "/assets/fonts/MPLUS1p-Regular.woff",
-  "/assets/fonts/SourceCodePro-Regular.woff",
-  "/assets/fonts/Ubuntu-Regular.woff",
-];
+const CacheVersion = '2019-08-29T10:43:34+09:00';
+const UrlsToCache = ['/'];
 
-self.addEventListener("install", (event) => {
-  console.log("ServiceWorker installing.");
+self.addEventListener('install', (event) => {
+  console.log('ServiceWorker installing.');
   event.waitUntil(
     caches
       .open(CacheVersion)
       .then((cache) => {
-        console.log("Opened cache");
+        console.log('Opened cache');
         return cache.addAll(UrlsToCache);
       })
       .then(self.skipWaiting),
   );
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("ServiceWorker activating.");
+self.addEventListener('activate', (event) => {
+  console.log('ServiceWorker activating.');
   event.waitUntil(
     caches.keys().then((keyList) => {
       keyList.forEach((key) => {
         if (CacheVersion !== key) {
-          console.log("Cache delete:", key);
-          caches.delete(key);
+          caches.delete(key).then(() => {
+            console.log('Cache deleted:', key);
+          });
         }
       });
     }),
   );
 });
 
-self.addEventListener("fetch", (event) => {
-  event.respondWith(
-    new Promise((resolve) => {
-      caches.match(event.request).then((cacheResponse) => {
-        if (cacheResponse) {
-          resolve(cacheResponse);
-          return;
-        }
-
-        const fetchRequest = event.request.clone();
-        fetch(fetchRequest)
-          .then((fetchResponse) => {
-            if (!fetchResponse || fetchResponse.status !== 200 || fetchResponse.type !== "basic") {
-              resolve(fetchResponse);
-              return;
-            }
-
-            const responseToCache = fetchResponse.clone();
-            caches.open(CacheVersion).then((cache) => {
-              cache.put(event.request, responseToCache);
-              resolve(fetchResponse);
-            });
-          })
-          .catch(() => {
-            resolve();
+self.addEventListener('fetch', (event) => {
+  event.respondWith(new Promise((resolve) => {
+    const { request } = event;
+    caches.match(request).then((cache) => {
+      fetch(request).then((response) => {
+        if (response != null && response.status === 200 && response.type === 'basic') {
+          caches.open(CacheVersion).then((cache) => {
+            cache.put(event.request, response.clone());
+            resolve(response);
           });
+        } else {
+          resolve(cache);
+        }
+      }).catch((err) => {
+        resolve(cache);
       });
-    }),
-  );
+    });
+  }));
 });
