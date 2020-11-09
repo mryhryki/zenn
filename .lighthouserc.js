c72c19531b15390827131a0d82b51b4ec3ff3f3f
hyiromori <hyiromori@gmail.com>
2020-11-09 17:50:08 +0900

    Add Lighthouse CI assertions.

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -8,5 +8,8 @@ module.exports = {
       serverBaseUrl: "https://lighthouse-hyiromori.herokuapp.com",
       token: "df7210bf-1658-4cd4-9b5b-9d97dc2646aa",
     },
+    assert: {
+      preset: "lighthouse:recommended",
+    },
   },
 };
