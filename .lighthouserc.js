4e3a32a3f39ee428075ed468e4e9caf871e8b5c1
hyiromori <hyiromori@gmail.com>
2020-11-10 17:28:05 +0900

    Update lighthouse ci setting.

--- .lighthouserc.js
+++ .lighthouserc.js
@@ -9,7 +9,13 @@ module.exports = {
       token: "df7210bf-1658-4cd4-9b5b-9d97dc2646aa",
     },
     assert: {
-      preset: "lighthouse:recommended",
+      preset: "lighthouse:no-pwa",
+      assertions: {
+        "categories:performance": ["error", {"minScore": 0.9}],
+        "categories:accessibility": ["error", {"minScore": 1}],
+        "unsized-images": "off",
+        "image-size-responsive": "off"
+      }
     },
   },
 };
