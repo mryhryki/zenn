013c9728293eab611c8e6d5e36557ac0c84673db
mryhryki <mryhryki@gmail.com>
2022-02-15 17:33:06 +0900

    Fix add reading log workflow

--- script/reading_log/add_reading_log.sh
+++ script/reading_log/add_reading_log.sh
@@ -1,13 +1,15 @@
 #!/usr/bin/env bash
+set -xe
 
 REPOSITORY_ROOT="$(git rev-parse --show-toplevel 2>/dev/null;)"
 READING_LOG_ID="$(date +"%Y%m%d-%H%M%S")"
+BRANCH_NAME="reading_log/${READING_LOG_ID}"
 TITLE="Add reading log: ${READING_LOG_ID}"
 
 echo "LOG_ID: ${READING_LOG_ID}"
-git checkout -b "reading_log/${READING_LOG_ID}"
+git checkout -b "${BRANCH_NAME}"
 
-cat << EOS > "${REPOSITORY_ROOT}/posts/reading_log/${LOG_ID}.md"
+cat << EOS > "${REPOSITORY_ROOT}/posts/reading_log/${READING_LOG_ID}.md"
 ---
 title: {{TITLE}}
 ---
@@ -15,8 +17,17 @@ title: {{TITLE}}
 
 EOS
 
-git commit -a -m "${TITLE}"
-git push
+# for GitHub Actions
+if [[ "$(git config --global user.email)" == "" ]]; then
+  git config --global user.email "mryhryki@gmail.com"
+fi
+if [[ "$(git config --global user.name)" == "" ]]; then
+  git config --global user.name "Moriya Hiroyuki"
+fi
+
+git add -A
+git commit -m "${TITLE}"
+git push --set-upstream origin "${BRANCH_NAME}"
 
 gh pr create \
   --title "${TITLE}" \
