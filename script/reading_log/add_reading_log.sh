77de7bde4ba6cc38fbf4d0ff036333fe85afc080
mryhryki <mryhryki@gmail.com>
2022-02-15 17:17:52 +0900

    Remove invalid file

--- script/reading_log/add_reading_log.sh
+++ script/reading_log/add_reading_log.sh
@@ -2,6 +2,7 @@
 
 REPOSITORY_ROOT="$(git rev-parse --show-toplevel 2>/dev/null;)"
 READING_LOG_ID="$(date +"%Y%m%d-%H%M%S")"
+TITLE="Add reading log: ${READING_LOG_ID}"
 
 echo "LOG_ID: ${READING_LOG_ID}"
 git checkout -b "reading_log/${READING_LOG_ID}"
@@ -14,5 +15,10 @@ title: {{TITLE}}
 
 EOS
 
-git commit -a -m "Add reading log: ${READING_LOG_ID}"
+git commit -a -m "${TITLE}"
 git push
+
+gh pr create \
+  --title "${TITLE}" \
+  --body " " \
+  --reviewer "mryhryki"
