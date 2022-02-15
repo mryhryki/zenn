6e2f41acc23d7482f232d093a565dba94fbc0fee
mryhryki <mryhryki@gmail.com>
2022-02-15 17:10:38 +0900

    Add reading log: 20220215-171038

--- script/reading_log/add_reading_log.sh
+++ script/reading_log/add_reading_log.sh
@@ -3,7 +3,6 @@
 REPOSITORY_ROOT="$(git rev-parse --show-toplevel 2>/dev/null;)"
 READING_LOG_ID="$(date +"%Y%m%d-%H%M%S")"
 
-echo "LOG_ID: ${READING_LOG_ID}"
 git checkout -b "reading_log/${READING_LOG_ID}"
 
 cat << EOS > "${REPOSITORY_ROOT}/posts/reading_log/${LOG_ID}.md"
@@ -16,3 +15,8 @@ EOS
 
 git commit -a -m "Add reading log: ${READING_LOG_ID}"
 git push
+echo "${READING_LOG_ID}"
+
+gh pr create \
+  --title "Reading log: ${READING_LOG_ID}" \
+  --reviewer "mryhryki"
