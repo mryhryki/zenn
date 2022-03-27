91e446b43023107de0e3c13cb5dafd80ecced6f3
mryhryki <mryhryki@gmail.com>
2022-03-27 09:36:37 +0900

    Fix blog post format

--- script/site/build/blog.ts
+++ script/site/build/blog.ts
@@ -2,14 +2,14 @@ import RSS from "rss";
 import path from "node:path";
 import { BaseURL, DestinationBlogDir } from "../util/definition";
 import { Post } from "../util/post";
-import { renderBlogIndex } from "../html/blog";
+import { renderBlogIndex, renderBlogPost } from "../html/blog";
 import { renderSlide } from "../html/slide";
 import { writeFile } from "node:fs/promises";
 
 export const buildBlog = async (blogPosts: Post[]): Promise<void> => {
   await Promise.all(
     blogPosts.map(async (blogPost): Promise<void> => {
-      await writeFile(path.resolve(DestinationBlogDir, `${blogPost.id}.html`), renderSlide(blogPost.markdown));
+      await writeFile(path.resolve(DestinationBlogDir, `${blogPost.id}.html`), renderBlogPost(blogPost));
     })
   );
   await writeFile(path.resolve(DestinationBlogDir, "index.html"), renderBlogIndex(blogPosts));
