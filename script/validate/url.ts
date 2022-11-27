d2df5ce6be22e66c62be74bacda7577ca8d17554
mryhryki <mryhryki@gmail.com>
2022-11-28 06:07:49 +0900

    Update script/validate/url.ts

--- script/validate/url.ts
+++ script/validate/url.ts
@@ -54,12 +54,13 @@ const main = async () => {
     }
   }));
 
-  if (urlInfoList.length > 0) {
+  const invalidUrls = urlInfoList.filter(({ status }) => status !== 200)
+  if (invalidUrls.length > 0) {
     console.log("[Invalid URLs]");
-    urlInfoList.filter(({ status }) => status !== 200).forEach((urlInfo) => {
+    invalidUrls.forEach((urlInfo) => {
       console.log(urlInfo.status, urlInfo.url.toString(), urlInfo.file);
     });
-    throw new Error("Exists invalid URLs")
+    throw new Error("Exists invalid URLs");
   }
 };
 
@@ -68,17 +69,25 @@ const getNow = (): number => new Date().getTime();
 
 const UnsupportedHeadRequestOrigins = new Set(["https://www.recordchina.co.jp", "https://jp.yna.co.kr"]);
 const validateUrl = async (url: URL): Promise<{ status: number }> => {
-  const abortController = new AbortController();
-  const timeoutId = setTimeout(() => abortController.abort(), 10000);
-  const response = await fetch(url, {
-    method: UnsupportedHeadRequestOrigins.has(url.origin) ? "GET" : "HEAD",
-    headers: {
-      "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
-    },
-    signal: abortController.signal
-  });
-  clearTimeout(timeoutId);
-  return { status: response.status };
+  for (let i = 0; i < 3; i++) {
+    try {
+
+      const abortController = new AbortController();
+      const timeoutId = setTimeout(() => abortController.abort(), 10000);
+      const response = await fetch(url, {
+        method: UnsupportedHeadRequestOrigins.has(url.origin) ? "GET" : "HEAD",
+        headers: {
+          "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/107.0.0.0 Safari/537.36"
+        },
+        signal: abortController.signal
+      });
+      clearTimeout(timeoutId);
+      return { status: response.status };
+    } catch (err) {
+      console.warn("WARN:", url, err);
+    }
+  }
+  return { status: 998 };
 };
 
 const startTime = getNow();
@@ -90,5 +99,5 @@ main()
   .catch((err) => {
     console.error("VALIDATE URL FAILED");
     console.error(err);
-    process.exit(1)
-  })
+    process.exit(1);
+  });
