57ce97bdf1b28fcb6d1d4c4414560c58787bb74c
mryhryki <mryhryki@gmail.com>
2023-01-04 08:17:53 +0900

    Revert "Remove add memo and scrap workflows"
    This reverts commit bdadb8429ad0cfe4b378fe5ca2de492d707f8363.

---
+++ .github/script/shell/add_scrap.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+REPOSITORY_ROOT="$(git rev-parse --show-toplevel 2>/dev/null;)"
+SCRAP_DIR="$(date +"%Y-%m")"
+SCRAP_ID="$(date +"%Y%m%d-%H%M%S")"
+BRANCH_NAME="scrap/${SCRAP_ID}"
+
+TITLE="${SCRAP_TITLE:-"${SCRAP_ID}"}"
+TEXT="$(printf '%s' "${SCRAP_TEXT:-"(TODO: TEXT)"}")"
+PR_TITLE="[SCRAP] ${TITLE}"
+
+echo "SCRAP_ID: ${SCRAP_ID}"
+git checkout -b "${BRANCH_NAME}"
+
+mkdir -p "${REPOSITORY_ROOT}/scrap/${SCRAP_DIR}"
+cat << EOS > "${REPOSITORY_ROOT}/scrap/${SCRAP_DIR}/${SCRAP_ID}.md"
+---
+title: ${TITLE}
+---
+
+${TEXT}
+
+EOS
+
+source "${REPOSITORY_ROOT}/.github/script/shell/common/setup_git.sh"
+git add -A
+git commit -m "${PR_TITLE}"
+git push --set-upstream origin "${BRANCH_NAME}"
+
+gh pr create \
+  --title "${PR_TITLE}" \
+  --body "${TEXT}" \
+  --reviewer "mryhryki"
