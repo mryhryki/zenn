b6446ed76680f0c4b182725e0cd9ea94a1f99807
mryhryki <mryhryki@gmail.com>
2023-01-29 01:38:52 +0900

    Add site/sw.js

---
+++ site/sw.js
@@ -0,0 +1,19 @@
+const CacheName = "v1";
+
+// https://jakearchibald.com/2014/offline-cookbook/#stale-while-revalidate
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    (async () => {
+      const cache = await caches.open(CacheName);
+      const cachedResponse = await cache.match(event.request);
+      const networkResponsePromise = fetch(event.request);
+      event.waitUntil(
+        (async () => {
+          const networkResponse = await networkResponsePromise;
+          await cache.put(event.request, networkResponse.clone());
+        })()
+      );
+      return cachedResponse || networkResponsePromise;
+    })()
+  );
+});
